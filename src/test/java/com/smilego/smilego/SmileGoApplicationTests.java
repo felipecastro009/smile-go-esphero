864f378d87642dbd85a0a850ebd6217e96a03a23
Felipe Castro <engfelipecastro@gmail.com>
2024-11-25 12:04:30 -0300

    feat: mock services em SmileGoApplicationTests

--- src/test/java/com/smilego/smilego/SmileGoApplicationTests.java
+++ src/test/java/com/smilego/smilego/SmileGoApplicationTests.java
@@ -1,14 +1,27 @@
 package com.smilego.smilego;
 
+import jakarta.activation.DataSource;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.core.RedisTemplate;
 
-@WebMvcTest(SmileGoApplicationTests.class)
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class SmileGoApplicationTests {
+    @Mock
+    private DataSource dataSource;
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+    @Mock
+    private RedisTemplate<String, Object> redisTemplate;
+    @Mock
+    private CacheManager cacheManager;
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
