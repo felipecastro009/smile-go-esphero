cda44fcd608ba32d9e65d7cd181cf605d3542396
Felipe Castro <engfelipecastro@gmail.com>
2024-11-26 07:13:37 -0300

    fix: swagger

--- src/main/java/com/smilego/smilego/main/SwaggerConfig.java
+++ src/main/java/com/smilego/smilego/main/SwaggerConfig.java
@@ -1,11 +1,13 @@
 package com.smilego.smilego.main;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
